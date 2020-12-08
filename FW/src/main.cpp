5755f4b7f1d572b0bae5d8f2a5d62c4f5cb46cf2
Rune Bekkevold <rune.bekkevold@gmail.com>
2020-12-08 20:05:48 +0100

    commit message

---
+++ FW/src/main.cpp
@@ -0,0 +1,141 @@
+/*
+ * This code demostrates a low frequency lockin amplifier or synchronous ligth 
+ * detector inplementation using only a Arduino board and 2 additional modules:
+ *   1. Atmega328p Arduino board w synchronous detector inplementation in code
+ *   2. The OPT101 amplified vissible ligth photodiode 
+ *   3. A laser pointer 
+ * 
+ * The syncronous detector components ar all realised in software 
+ * using the Atmega328p builtin ADC as frontend.
+ * 
+ * Basic spec
+ * ----------
+ * ADC raw samplerate is 78.6kHz with estimated > 8 bit resoluition. 
+ *    LSB is a bit unstable becuse of the high samplerate but is giving better relative 
+ *    resolution wit this compared to lovering the samplerate and decimation ration
+ * ADC resolution is >14 bit after oversampoling and decimation
+ * Laser modulation or exitation frequency 12.8kHz, and so also the LO frequency of the sychronous detector.
+ * After decimation the output samplerate from the detector is 12.8 / 4^6 = 3.125 kS/s
+ * Ewerything is divided down and synchronous to the ADC raw samplerate clock.
+ * 
+ * Copyright (c) 2020 Rune Bekkevold
+ * Released under the terms of the MIT license:
+ * https://opensource.org/licenses/MIT
+ * 
+ */
+
+#include <Arduino.h>
+
+// Analog input connected to the photo sensor
+const uint8_t sensorPin = 0;
+const uint8_t ExPin = 2; // Using D2 pin for Laser exitation output
+
+// ADC resolition ioncrease bu oversampling an decimation
+const uint8_t ADCincreasedRes = 6;                                  // adding 6 extra bits ADC resolution
+const uint16_t decimationFactor = (int)pow(4, ADCincreasedRes) - 1; // decimate oversampled data by 4^increasedRes
+
+// Light readings provided by the ADC interrupt service routine.
+volatile int32_t lightReading;
+volatile int32_t backGroundLvlReading;
+volatile bool ligthReadingReady;
+
+// ADC interrupt service routine.
+// Called each time a conversion result is available.
+ISR(ADC_vect)
+{
+    // Read the ADC.
+    int32_t sample = ADC;
+    static uint16_t sample_count;
+    static uint32_t backgroundLvlAcc;
+
+    static bool ExitationOn;
+
+    // Toggle exitation and sample sign as fast as possible inside our sensor bandwith
+    // Default 3dB bandwith for the opt101 is 14kHz so we need to be a bit slower than that
+    // This is a simple square wave mixer, I supose we could experiment with a sinewave LUT here
+    // but then how can we avoid floating point to make it fast enough?
+    if (!ExitationOn)
+    { // If exitation was of for this sample, invert the sample
+        backgroundLvlAcc += sample;
+        sample = -sample;
+    }
+
+    // Set up our exitation state for the next 6 samples
+    // for 78.6 kS/s samplerate, division by 6 gives 12.8kHz on the exitation pin,
+    // so well within the bandwith of the OPT101 sensor.
+    if ((sample_count & 0b111) == 6) // toggle exitation everi 6th sample
+    {
+        if (!ExitationOn)
+        {
+            PORTD = PORTD | 0b00000100; // set exitation pin D2 high
+            ExitationOn = true;
+        }
+        else
+        {
+            PORTD = PORTD & 0b11111011; // set exitation pin D2 low
+            ExitationOn = false;
+        }
+    }
+
+    static int32_t ADCsampleAccumulator;
+    ADCsampleAccumulator += sample; // sum up our samples for one decimation lenght
+
+    if ((sample_count & decimationFactor) == 0)
+    { // everytime sample counter reaches decimationFactor number of samples
+        if (ADCsampleAccumulator > 0)
+        { // Accumulator value is posiotve
+            lightReading = (int32_t)(((uint32_t)ADCsampleAccumulator) >> ADCincreasedRes);
+        }
+        else
+        { // Accumulator value is negative, flip sign and cast acc to unsigned before right shift
+            lightReading = -(int32_t)(((uint32_t)-ADCsampleAccumulator) >> ADCincreasedRes);
+        }
+        ADCsampleAccumulator = sample; // reset sample accumulator to previous sample;
+
+        // get ambient light level
+        backGroundLvlReading = (backgroundLvlAcc >> (ADCincreasedRes - 1)); // the ambient level measurement is only haf the number of samples
+        backgroundLvlAcc = 0;                                               // reset ambient Acc to 0 //(backGroundLvlReading >> ADCincreasedRes);
+        ligthReadingReady = true;
+    }
+
+    sample_count++;
+}
+
+void setup()
+{
+    // Configure the ADC in "free running mode".
+    ADMUX = _BV(REFS0)    // ref = AVCC
+            | sensorPin;  // input channel
+    ADCSRB = 0;           // free running mode
+    ADCSRA = _BV(ADEN)    // enable
+             | _BV(ADSC)  // start conversion
+             | _BV(ADATE) // auto trigger enable
+             | _BV(ADIF)  // clear interrupt flag
+             | _BV(ADIE)  // interrupt enable
+             | 4;         //4  prescale 16 //5; // prescaler 32. 7;  // prescaler = 128.
+
+    // Configure the serial port.
+    Serial.begin(115200);
+    // configure Exitation pin mode and initial state;
+    pinMode(ExPin, OUTPUT);
+    digitalWrite(ExPin, HIGH);
+}
+
+void loop()
+{
+    // Wait for interupt action to hsppen and saying a reading a reading is available.
+    while (!ligthReadingReady) /* wait */
+        ;
+
+    // Get a copy of the detecor result
+    noInterrupts();
+    int32_t lighthReading_copy = lightReading;
+    int32_t backGroundLvlReading_copy = backGroundLvlReading;
+    ligthReadingReady = false;
+    interrupts();
+
+    // Transmit data.
+    Serial.print(lighthReading_copy);           // Transmit detection level 
+    Serial.print(",");                  
+    Serial.println(backGroundLvlReading_copy);  // Transmit ambient level
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Arduino Synchronous Detector
+
+
+## Basic principle
+
+![Basic principle](specs/lockin_detector-Signals.png)
+
+
+
+## Analog implementation
+![Analog Synchronous detector](specs/Analog_detector.png)
+
+
+
+## Full software inplementation using Arduino
+![Arduino Synchronous detector](specs/Arduino_detector.png)
+
+
+
+## Description
+This code demostrates a low frequency lockin amplifier or synchronous ligth detector inplementation using only a Arduino board and 2 additional modules:
+  1. Atmega328p Arduino board w synchronous detector inplementation in code
+  2. The OPT101 amplified vissible ligth photodiode 
+  3. A laser pointer 
+  
+The syncronous detector components ar all realised in software 
+using the Atmega328p builtin ADC as frontend.
+
+  
+
+##  Basic specs (work in progress)
+ADC raw samplerate is 78.6kHz with estimated > 8 bit resoluition. 
+(LSB is a bit unstable becuse of the high samplerate)
+ADC resolution is >14 bit after oversampoling and decimation
+Laser modulation or exitation frequency 12.8kHz, and so also the LO frequency of the sychronous detector.
+After decimation the output samplerate from the detector is 12.8 / 4^6 = 3.125 kS/s
+Ewerything is divided down and synchronous to the ADC raw samplerate clock.
+  
+  
