1607272dd6b0678095b2949df0fbb5e64a6cf725
Rune Bekkevold <rune.bekkevold@gmail.com>
2020-12-13 00:40:23 +0100

    uppdated drawing

--- FW/src/main.cpp
+++ FW/src/main.cpp
@@ -32,7 +32,7 @@ const uint8_t ExPin = 2; // Using D2 pin for Laser exitation output
 
 // ADC resolition ioncrease by oversampling an decimation
 const uint8_t ADCincreasedRes = 6;                                  // adding 6 extra bits ADC resolution
-const uint16_t decimationFactor = (int)pow(4, ADCincreasedRes) - 1; // decimate oversampled data by 4^increasedRes
+const uint16_t decimationFactor = (int)pow(4, ADCincreasedRes); // decimate oversampled data by 4^increasedRes
 
 // Light readings provided by the ADC interrupt service routine.
 volatile int32_t lightReading;              // Our detectir reading result goes here
@@ -45,10 +45,13 @@ ISR(ADC_vect)
 {
     // Read a sample from the ADC.
     int32_t sample = ADC;
-    static uint16_t sample_count;
+    static uint16_t decimationCounter;
+    static uint8_t exitationCounter;
     static uint32_t backgroundLvlAcc;
 
     static bool ExitationOn;
+    decimationCounter++;
+    exitationCounter++;
     
     // Mixer Function:
     // Toggle exitation and sample sign as fast as possible inside our sensor bandwith
@@ -58,13 +61,13 @@ ISR(ADC_vect)
     if (!ExitationOn)
     { // If exitation was off when the current sample was aquired, invert the sample sign
         backgroundLvlAcc += sample;   // lets also get a measurment of the ambient ligth level by averaging only the samples when the exitation is off
-        sample = -sample;             // invert sample sign.
+        sample = -sample;             // invert sample sign. this migth need some delay to aligne with the delayed responce from the sensor
     }
 
     // Set up our exitation state for the next 6 samples
-    // because for 78.6 kS/s samplerate, division by 6 gives 12.8kHz on the exitation pin,
+    // because for 76.9 kS/s samplerate, division by 12 gives 6.41kHz on the exitation pin,
     // so shold be well within the bandwith of the OPT101 sensor.
-    if ((sample_count & 0b111) == 6) // toggle exitation every 6th sample This wil actually be divide by 7
+    if (exitationCounter == 6)     // toggle exitation every 6. sample
     {
         if (!ExitationOn)
         {
@@ -76,15 +79,16 @@ ISR(ADC_vect)
             PORTD = PORTD & 0b11111011; // set exitation pin D2 low
             ExitationOn = false;
         }
+        exitationCounter = 0;
     }
 
     static int32_t ADCsampleAccumulator;
     ADCsampleAccumulator += sample; // sum up our samples for one decimation lenght
 
-    if ((sample_count & decimationFactor) == 0) // This decimation length mask wil inly work as intended if it is power of 2, need to consider this
+    if (decimationCounter == decimationFactor) 
     { // everytime sample counter reaches decimationFactor number of samples
         if (ADCsampleAccumulator > 0)
-        { // Accumulator value is posiotve
+        {   // Accumulator value is posiotve. cast acc to uint and rigthshift with virtual resolution invrease
             lightReading = (int32_t)(((uint32_t)ADCsampleAccumulator) >> ADCincreasedRes);
         }
         else
@@ -94,12 +98,11 @@ ISR(ADC_vect)
         ADCsampleAccumulator = sample; // reset sample accumulator to previous sample;
 
         // get ambient light level
-        backGroundLvlReading = (backgroundLvlAcc >> (ADCincreasedRes - 1)); // the ambient level measurement is only haf the number of samples
+        backGroundLvlReading = (backgroundLvlAcc >> (ADCincreasedRes - 1)); // the ambient level measurement is only haf the number of samples so we shist one bit less
         backgroundLvlAcc = 0;                                               // reset ambient Acc to 0 //(backGroundLvlReading >> ADCincreasedRes);
         ligthReadingReady = true;
+        decimationCounter = 0;
     }
-
-    sample_count++;
 }
 
 void setup()
@@ -136,7 +139,9 @@ void loop()
     interrupts();
 
     // Transmit data.
-    Serial.print(lighthReading_copy);           // Transmit detection level 
+    Serial.print(backGroundLvlReading_copy);         // Transmit detection level 
+    //Serial.print(",");
+    //Serial.print(150);
     Serial.print(",");                  
-    Serial.println(backGroundLvlReading_copy);  // Transmit ambient level
+    Serial.println(lighthReading_copy);  // Transmit ambient level
 }
